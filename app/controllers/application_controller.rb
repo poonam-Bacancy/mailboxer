ed3765f5117ccc1f881281004f68cd6acfe012af
poonam-Bacancy <poonam.shah@bacancytechnology.com>
2018-03-15 14:06:28 +0530

    added all the files into the mailboxer

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  	rescue_from ActiveRecord::RecordNotFound do
+  		flash[:warning] = 'Resource not found.'
+  		redirect_back_or root_path
+	end
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def redirect_back_or(path)
+  		redirect_to request.referer || path
+	end
+
+	protected
+
+	def configure_permitted_parameters
+  		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  		# devise_parameter_sanitizer.for(:sign_up) << :name
+  		# devise_parameter_sanitizer.for(:account_update) << :name
+	end
+end
